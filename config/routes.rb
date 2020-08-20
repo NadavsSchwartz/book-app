1f1d2cb615c310a938654a18d1add41aed21ddee
Morgan VanYperen <morgvanny@gmail.com>
2020-08-19 18:51:42 -0700

    next

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+
+  post '/signin' to: 'sessions#create'
+
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
