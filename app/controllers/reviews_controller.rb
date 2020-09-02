0a217cd068626638d44098504d61e1d9d40b2e12
Morgan VanYperen <morgvanny@gmail.com>
2020-09-01 17:12:18 -0700

    sg

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,36 @@
 class ReviewsController < ApplicationController
+  def index
+    @book = Book.find(params[:book_id])
+
+    @reviews = @book.reviews
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
 
   def new
+    @book = Book.find(params[:book_id])
     @review = Review.new
-    @book_id = params[:book]
-    @books = Book.all
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to book_reviews_path(@review.book)
+    else
+      render :edit
+    end
   end
 
   def create
-    @review = current_user.reviews.build(review_params)
+    @review = current_user.reviews.build(**review_params, book_id: params[:book_id])
     if @review.save
-      redirect_to @review.book, notice: "Review successfully created."
+      redirect_to redirect_to book_reviews_path(@review.book), notice: "Review successfully created."
     else
       render :new
     end
@@ -18,6 +39,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :book_id, :content)
+    params.require(:review).permit(:rating, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
   resources :users
 
   post '/signin', to: 'sessions#create'
 
-  resources :books
+  resources :books do
+    resources :reviews, shallow: true
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
